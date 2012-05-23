5b20a52c5b7052319fcceb62cc4de861e5bac30e
Carmen Johnson <blackpalmmobileapplications@gmail.com>
2012-05-22 22:13:18 -0700

    Finished String Library

--- Johnson_Carmen_Assignment4.js
+++ Johnson_Carmen_Assignment4.js
@@ -32,6 +32,39 @@ StringLibrary.prototype.isEmail = function(arg) {
 	var lower = arg.toLowerCase();
 	if ( emailPattern.test(lower) )
 		return true;
+		
 	else
 		return false;
 };
+
+// Function to check whether given string is a URL
+StringLibrary.prototype.isURL = function(arg) {
+	if ( arg.search("http:") == 0 || arg.search("https:") == 0 )
+		return true;
+	else
+		return false;
+};
+
+// Function to capitalize first letter of each word in a given string
+StringLibrary.prototype.makeTitleCase = function(arg) {
+	var string = "";
+	var words = arg.split(" ");
+	for (i=0; i<words.length; i++) {
+		var word = words[i];
+		string += word.substr(0,1).toUpperCase() + word.substr(1, word.length) + ' ';
+	}
+	return string;
+}
+
+// Function to replace a character with another in a given string
+StringLibrary.prototype.replaceInString = function( arg, character, replacementChar ) {
+	var string = "";
+	
+	for (i=0; i<arg.length; i++) {
+		if (arg[i] === character)
+			string += replacementChar;
+		else
+			string += arg[i];
+	}
+	return string;
+}
