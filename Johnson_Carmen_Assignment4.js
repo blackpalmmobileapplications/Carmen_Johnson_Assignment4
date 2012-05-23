7b3d7083c4c1701c995feadbc36064016b72bbc8
Carmen Johnson <blackpalmmobileapplications@gmail.com>
2012-05-22 22:03:01 -0700

    completed first 3 functions from String Library

--- Johnson_Carmen_Assignment4.js
+++ Johnson_Carmen_Assignment4.js
@@ -11,3 +11,27 @@ alert("JavaScript works!");
 StringLibrary = function() {
   emailPattern = /[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}/;
 }
+
+/* PUBLIC METHODS OF THE STRING LIBRARY */
+// Log function
+StringLibrary.prototype.log = function(output) {
+  return console.log(output);
+};
+
+// Function to check whether given string is in form of a phone number: ###-###-####
+StringLibrary.prototype.isPhoneNumber = function(arg) {
+  var numbers = arg.split('-');
+	if (numbers.length == 3  &&  numbers[0].length == 3 && numbers[1].length == 3 && numbers[2].length == 4)
+		return true;
+	else
+		return false;
+};
+
+// Function to check whether given string is an e-Mail address
+StringLibrary.prototype.isEmail = function(arg) {
+	var lower = arg.toLowerCase();
+	if ( emailPattern.test(lower) )
+		return true;
+	else
+		return false;
+};
